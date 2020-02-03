a6600c2ca82050615c1a4b42b694cfc91e0e5abf
Konrad Kollnig <5175206+kasnder@users.noreply.github.com>
2020-02-03 15:25:01 +0000

    Support for token iteration

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ Stores email-password pairs, gives out Google Play Store tokens and GSFid.
 
 This repository ports the https://github.com/yeriomin/token-dispenser to Python. This is due to problems with using the existing API.
 
-At the moment, only one email-password pair is supported.
-
-In the future, this behaviour will be changed to use a random couple (login, password) from the provided ones, to load balance over multiple accounts, mitigating account ban possibilities.
+A  random couple (login, password) from `passwords/passwords.txt` is used, to load balance over multiple accounts, mitigating account ban possibilities.
 
 ## Usage
 
-`python3 token_dispenser.py`
+1. Set up `passwords/passwords.txt`.
+2. Run `python3 token_dispenser.py`, creating a token dispenser at `http://localhost:8080/`.
+3. If required, the address, port, locale, timezone, and used device can be changed directly in the Python code.
+
+## Troubleshooting
+
+Often, the token generation will fail. In these cases, one has to manually visit (sometimes multiple times) the following website, after which the process can continue:
+
+https://accounts.google.com/b/0/DisplayUnlockCaptcha
--- token_dispenser.py
+++ token_dispenser.py
@@ -1,37 +1,32 @@
 import http.server
 import socketserver
 from gpapi.googleplay import GooglePlayAPI, LoginError, SecurityCheckError
-import configparser
+import random
 
 class TokenDispenser(socketserver.BaseRequestHandler):
-    def setup(self):
-        config = configparser.ConfigParser()
-        config.read('gplaycli.conf')
+	def login(self):
+		return random.choice(list(open('passwords/passwords.txt'))).split(" ")
 
-        self.gmail_address      = config.get('Credentials', 'gmail_address', fallback=None)
-        self.gmail_password     = config.get('Credentials', 'gmail_password', fallback=None)
-        self.device_codename    = config.get('Device', 'codename', fallback='bacon')
-        self.locale             = config.get("Locale", "locale", fallback="en_GB")
-        self.timezone           = config.get("Locale", "timezone", fallback="CEST")
+	def setup(self):
+		self.api = GooglePlayAPI(locale="en_GB", timezone="CEST", device_codename="bacon")
 
-        self.api = GooglePlayAPI(locale=self.locale, timezone=self.timezone, device_codename=self.device_codename)
+	def handle(self):
+		try:
+			self.gmail_address, self.gmail_password = self.login()
+			self.api.login(email=self.gmail_address, password=self.gmail_password)
+			self.token = self.api.authSubToken
+			self.gsfid = hex(self.api.gsfId).replace('0x', '')
 
-    def handle(self):
-        try:
-            self.api.login(email=self.gmail_address, password=self.gmail_password)
-            self.token = self.api.authSubToken
-            self.gsfid = hex(self.api.gsfId).replace('0x', '')
-
-            print("Token: {}, gsfId: {}".format(self.token, self.gsfid))
-            self.request.sendall(str.encode(self.token + " " + self.gsfid)) # self.data.upper()
-        except (LoginError, SecurityCheckError) as e:
-            print("Error: " + str(e))
-            self.request.sendall(str.encode(str(e)))
+			print("Token: {}, gsfId: {}".format(self.token, self.gsfid))
+			self.request.sendall(str.encode(self.token + " " + self.gsfid))
+		except (LoginError, SecurityCheckError) as e:
+			print("Error: " + str(e))
+			self.request.sendall(str.encode(str(e)))
 
 if __name__ == "__main__":
-    socketserver.TCPServer.allow_reuse_address = True # Prevent 'cannot bind to address' errors on restart
+	socketserver.TCPServer.allow_reuse_address = True # Prevent 'cannot bind to address' errors on restart
 
-    HOST, PORT = "localhost", 8080
+	HOST, PORT = "localhost", 8080
 
-    with socketserver.TCPServer((HOST, PORT), TokenDispenser) as server:
-        server.serve_forever()
+	with socketserver.TCPServer((HOST, PORT), TokenDispenser) as server:
+		server.serve_forever()
